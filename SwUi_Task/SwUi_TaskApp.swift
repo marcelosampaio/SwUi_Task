d85b17c4d29a4e186cabd157f4a1c9e1b9a59f33
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2022-12-04 14:29:40 -0300

    Initial Commit

---
+++ SwUi_Task/SwUi_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_TaskApp.swift
+//  SwUi_Task
+//
+//  Created by Marcelo Sampaio on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
